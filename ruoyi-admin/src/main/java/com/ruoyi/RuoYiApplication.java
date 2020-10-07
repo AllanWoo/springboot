60c8e0d1b46fd956efae2265ebb7fded26f6e3ad
AllanWoo <878190191@qq.com>
2020-10-07 11:54:53 +0800

    first commit logs

---
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,40 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class RuoYiApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println(" _ooOoo_\n" +
+                "                          o8888888o\n" +
+                "                          88\" . \"88\n" +
+                "                          (| -_- |)\n" +
+                "                          O\\  =  /O\n" +
+                "                       ____/`---'\\____\n" +
+                "                     .'  \\\\|     |//  `.\n" +
+                "                    /  \\\\|||  :  |||//  \\\n" +
+                "                   /  _||||| -:- |||||-  \\\n" +
+                "                   |   | \\\\\\  -  /// |   |\n" +
+                "                   | \\_|  ''\\---/''  |   |\n" +
+                "                   \\  .-\\__  `-`  ___/-. /\n" +
+                "                 ___`. .'  /--.--\\  `. . __\n" +
+                "              .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n" +
+                "             | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n" +
+                "             \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n" +
+                "        ======`-.____`-.___\\_____/___.-`____.-'======\n" +
+                "                           `=---='\n" +
+                "        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
+                "               // 佛祖保佑 永不宕机 永无BUG //");
+    }
+}
